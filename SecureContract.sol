21949a63a393f2d3a3b3b1fd5a8827a3b3011e75
Marcel Koopman <marcel@MacBook-Pro-van-Esther.local>
2017-02-09 16:29:37 +0100

    Voorbeeld contract

---
+++ SecureContract.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.9;
+
+/**
+ * Contract to store data in a secure manner.
+ **/
+contract SecureData {
+    
+    uint data;
+    address creator;
+    
+    function SecureData() {
+        creator = msg.sender;
+    }
+    
+    function setData(uint value) { 
+        verifyMsgSender();
+        data = value;
+    } 
+    
+    function getData() constant returns (uint) { 
+        verifyMsgSender();
+        return data; 
+    }
+    
+    function verifyMsgSender() {
+        if (msg.sender != creator) throw;
+    }
+}
+
