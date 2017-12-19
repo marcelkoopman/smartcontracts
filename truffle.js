3bf77104b8ffef88c68609684240633a045afee1
Marcel Koopman <koopman.m@tkppensioen.nl>
2017-12-19 11:17:04 +0100

    zeppelin

--- truffle.js
+++ truffle.js
@@ -1,4 +1,6 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  rpc: {
+    host: "localhost",
+    port: 8545
+  }
 };
